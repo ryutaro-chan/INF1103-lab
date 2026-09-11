f28c070845a3afcebe141cabb00040850530ccd0
Ryutaro Chan <ryutarochan8@gmail.com>
2026-09-11 17:04:37 +0800

    Reporting

--- auditor.py
+++ auditor.py
@@ -1,12 +1,17 @@
 inventory = 0
+Rejected = 0
 while True:
     userInput = input("Enter a stock quantity or type 'quit' to exit: ")
     if userInput == "quit":
+        print("Total Units Processed" + str(inventory))
+        print("Number of Failed/Rejected"+ str(Rejected)) 
         break
-    if userInput.isdigit() == False:
+    elif userInput.isdigit() == False:
+        Rejected += 1
         print("Please input a positive integer")
     else:
         inventory += int(userInput)
     if inventory > 500: 
         print ("Overstock") 
         break
+           
